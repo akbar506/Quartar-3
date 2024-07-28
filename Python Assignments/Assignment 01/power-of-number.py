1ce6e72df81f25195d9011b620fc57a2dec51d12
Akbar Ali <m.akberali507@gmail.com>
2024-07-28 23:21:58 +0500

    Calculate Powers of Numbers.

---
+++ Python Assignments/Assignment 01/power-of-number.py
@@ -0,0 +1,19 @@
+"""
+Calculate Powers of Numbers.
+
+- Task: Given two integers base and exponent
+ - Compute base raised to the power of exponent.
+
+ - base:int = 3
+ - exponent:int = 4
+
+- Expected Output:
+ - Power result: 81
+"""
+
+base:int = 3
+exponent:int = 4
+
+power: int = base**exponent # Output: Power result: 81
+
+print(f"Power result: {power}")
