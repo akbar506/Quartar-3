cfc21c1ec95c825c78d2f640caabe054d1a52cf7
Akbar Ali <m.akberali507@gmail.com>
2024-07-31 16:23:45 +0500

    Add two numbers

---
+++ Python Assignments/Assignment 02/add_two_numbers.py
@@ -0,0 +1,18 @@
+"""
+Write a Python program that takes two integer inputs from the user and calculates their sum. The program should perform the following tasks:
+
+- Prompt the user to enter the first number.
+- Read the input and convert it to an integer.
+- Prompt the user to enter the second number.
+- Read the input and convert it to an integer.
+- Calculate the sum of the two numbers.
+
+Print the total sum with an appropriate message.
+"""
+
+num1:int = int(input("Enter first number: "))
+num2:int = int(input("Enter second number: "))
+
+sum:int = num1 + num2
+
+print(f"Sum of two numbers is {sum}")
