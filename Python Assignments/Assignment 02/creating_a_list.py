3fca15f9cb5ea48f91d3eadffb62b9a6bbc482e6
Akbar Ali <m.akberali507@gmail.com>
2024-08-01 15:55:56 +0500

    Creating a list

---
+++ Python Assignments/Assignment 02/creating_a_list.py
@@ -0,0 +1,13 @@
+"""
+Creating a list You have two lists:
+
+- list1 with elements [1, 2, 3].
+- list2 with elements [4, 5, 6].
+
+- Create a program using list method to add the elements of list2 to list1.
+"""
+
+lst1:list[int] = [1, 2, 3]
+lst2:list[int] = [4, 5, 6]
+
+print(lst1+lst2)
