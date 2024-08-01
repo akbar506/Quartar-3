3d33551d05313db8cee80e3dcf9666c680585317
Akbar Ali <m.akberali507@gmail.com>
2024-08-01 15:28:55 +0500

    Delete a number

--- Python Assignments/Assignment 02/delete_a_number.py
+++ Python Assignments/Assignment 02/delete_a_number.py
@@ -4,11 +4,10 @@ Delete a number Consider a list named numbers with the elements [1, 2, 3, 4, 5].
 
 lst:list[int] = [1, 2, 3, 4, 5]
 
-
-
 # This remove method will remove first occurrence of value.
 lst.remove(3)
 print(lst) # Output: [1, 2, 4, 5]  
+
 
 lst:list[int] = [1, 2, 3, 4, 5]
 
