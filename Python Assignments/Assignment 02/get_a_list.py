84867776761a5d7f5ba887bdc2bf4231b4d1cf55
Akbar Ali <m.akberali507@gmail.com>
2024-08-01 16:46:11 +0500

    Get a List

---
+++ Python Assignments/Assignment 02/get_a_list.py
@@ -0,0 +1,22 @@
+"""
+Get a List Write a program which continuously asks the user to enter values which are added one by one into a list. When the user presses enter without typing anything, print the list.
+
+Here's a sample run (user input is in blue):
+
+- Enter a value: 1
+- Enter a value: 2
+- Enter a value: 3
+- Enter a value:
+
+- Here's the list: ['1', '2', '3']
+"""
+
+lst = []
+
+while True:
+    user_input = input("Enter a value: ")
+    if user_input == "":
+        break
+    lst.append(user_input)
+
+print(f"Here's the list: {lst}")
