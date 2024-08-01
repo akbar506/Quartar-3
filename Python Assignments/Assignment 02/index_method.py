4a625b8500650ec0384a17340b89f0b1ac98921c
Akbar Ali <m.akberali507@gmail.com>
2024-08-01 16:16:54 +0500

    Index Method

---
+++ Python Assignments/Assignment 02/index_method.py
@@ -0,0 +1,10 @@
+"""
+Index Method You have a list called colors with the elements ['red', 'blue', 'green', 'yellow']. If you use the index method to find the position of 'green', what will the index be?
+"""
+"""
+Answer: Index will be "2".
+"""
+colors:list[int] = ['red', 'blue', 'green', 'yellow']
+
+color_green_index = colors.index("green")
+print(color_green_index)
